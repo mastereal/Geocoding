eeb093e6ec034b589a3fbef6e0809da2c84a1de2
Mastereal <kxc543@student.bham.ac.uk>
2018-12-29 02:56:28 +0000

    Translate the column names of original data into English, and export it into .csv and .xlsx. This step will no longer repeat

---
+++ geocodingTest.py
@@ -0,0 +1,82 @@
+#from googletrans import Translator # "googletrans" is not stable
+from google.cloud import translate  # Use google cloud API instead
+import pandas as pd # pd is abbreviation of Pandas, easy to remember
+print("Every thing is OK. Programe is operated approximately") # A test for the script
+
+translate_client=translate.Client()
+
+ori_data = pd.read_excel("./data/flood_drought_CN.xlsx",header=0,sheet_name=0)
+# Input data from excel, since csv need encoding and would not drop vacant columns)
+print(ori_data) # Check for the data
+
+citylist=list(ori_data.columns.str.strip()) 
+# Extract columns names as string (strip for blanks), and then convert them into list.
+#print(citylist)
+
+
+
+citylist_EN=[]  # Prepare an empty list
+count=0
+def is_chinese(uchar):         
+    if '\u4e00' <= uchar<='\u9fff':
+        return True
+    else:
+        return False
+# Check if the variable is Chinese
+def is_other(uchar):
+    if not is_chinese(uchar):
+        return True
+    else:
+        return False
+# Check if the variable is not Chinese
+for city in citylist:
+    #translator=Translator() # Try to initialize
+    count +=1 # count in case some missing translation
+    dic_city=translate_client.translate(city)
+    # Translate very city in citylist
+    #print(t_city.text)
+    #print(type(t_city))
+    #print(count)
+    t_city=dic_city["translatedText"]
+    citylist_EN.append(str(t_city))
+        # Add translations into new list. Convert translations into string
+    #else:
+        #citylist_EN.append("0")
+        # Some words are not translated. Replace them with "0"
+       
+        # This try was failed in this sample 
+print(citylist_EN)
+
+i_replace_1=citylist_EN.index("Shijiazhuang")+1
+i_replace_2=citylist_EN.index("Fuyang")+1
+i_replace_3=citylist_EN.index("Xinyang")+1
+i_replace_4=citylist_EN.index("Ji County")
+i_replace_5=citylist_EN.index("Zhang Wei")
+i_replace_6=citylist_EN.index("Bailing Temple")
+i_replace_7=citylist_EN.index("Shaanxi Dam")
+i_replace_8=citylist_EN.index("Parry")
+i_replace_9=citylist_EN.index("Yue Yang")
+# Obtain the index, of which the translation did not success
+citylist_EN[i_replace_1]="Handan"
+citylist_EN[i_replace_2]="Bengbu"
+citylist_EN[i_replace_3]="Dezhou"
+citylist_EN[i_replace_4]="Yun County"
+citylist_EN[i_replace_5]="Zhangye"
+citylist_EN[i_replace_6]="Bailingmiao"
+citylist_EN[i_replace_7]="Shanbazhen"
+citylist_EN[i_replace_8]="Palizhen"
+citylist_EN[i_replace_9]="Yueyang"
+# Replace with correct translation
+print(citylist_EN)
+print(ori_data.columns)
+
+ori_data.columns=citylist_EN
+print(ori_data)
+# Change column names into English
+
+# Then all the cities were translated into English, and were in the list.
+# Next part is to Geocoding
+
+ori_data.to_csv("./data/flood_drought_EN.csv")
+ori_data.to_excel("./data/flood_drought_EN.xlsx")
+# In case of API failed. The data would be export to csv
