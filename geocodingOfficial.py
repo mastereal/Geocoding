4bc36c25177fdd7008380547f883160cb5fc1f95
Mastereal <kxc543@student.bham.ac.uk>
2018-12-29 05:23:00 +0000

    Access to geocoding and obtain latitude and longtitude of every city

---
+++ geocodingOfficial.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from pandas import DataFrame, Series
+ori_data = pd.read_excel("./data/flood_drought_EN.xlsx",header=0,sheet_name=0)
+# Import data
+citylist=list(ori_data.columns.str.strip())
+# Extract columns names as string (strip for blanks), and then convert them into list.
+import googlemaps
+gmaps=googlemaps.Client(key="AIzaSyC5OpS9gqtZqG1zWLsqs_sa8CuKysTpt3g")
+# Setup python client for geocoding API
+latitude=[]
+longtitude=[]
+count=0
+for city in citylist:
+    citycoding=gmaps.geocode(city)
+    count+=1
+    #print(count)
+    #print(citycoding)
+    #keylist=citycoding[0].keys()
+    #print(keylist)
+    if not citycoding==[]:
+        city_lat=citycoding[0]["geometry"]["location"]["lat"] # Extract latitude from result   
+        city_lng=citycoding[0]["geometry"]["location"]["lng"] # Extract longtitude from result
+        if 3<city_lat<60 and 70<city_lng<140:   # Select results that located in China
+            latitude.append(city_lat)
+            longtitude.append(city_lng)
+        else:
+            print(city) # Check with city that failed to code and the index of this city
+            index_ll=count-1
+            print(index_ll)
+            latitude.append(0)
+            longtitude.append(0)
+    else:   # Check with city that failed to code and the index of this city
+        print(city)
+        index_ll=count-1
+        print(index_ll)
+        latitude.append(0)
+        longtitude.append(0)
+
+latitude[14]=42.203591
+longtitude[14]=116.485555
+
+latitude[18]=39.08965
+longtitude[18]=107.97616
+
+latitude[26]=38.874434
+longtitude[26]=115.464589
+
+latitude[57]=27.087637
+longtitude[57]=114.964696
+
+latitude[97]=36.585445
+longtitude[97]=109.489757
+
+latitude[98]=34.341574
+longtitude[98]=108.93977
+
+latitude[112]=25.606486
+longtitude[112]=100.267638
+
+latitude[115]=22.78691
+longtitude[115]=100.977164
+
+
+
+datadict={"city":citylist,"latitude":latitude,"longtitude":longtitude} # Setup dictionary to apply DataFrame method
+datareconstruct=DataFrame(datadict, columns=["city","latitude","longtitude"])
+print(datareconstruct)
+
+datareconstruct.to_excel("./geocoding_result_floodanddrought.xlsx")
+datareconstruct.to_csv("./geocoding_result_floodanddrought.csv")
--- geocodingTest.py
+++ geocodingTest.py
@@ -1,6 +1,8 @@
 #from googletrans import Translator # "googletrans" is not stable
 from google.cloud import translate  # Use google cloud API instead
 import pandas as pd # pd is abbreviation of Pandas, easy to remember
+from pandas import DataFrame, Series
+
 print("Every thing is OK. Programe is operated approximately") # A test for the script
 
 translate_client=translate.Client()
@@ -77,6 +79,68 @@ print(ori_data)
 # Then all the cities were translated into English, and were in the list.
 # Next part is to Geocoding
 
-ori_data.to_csv("./data/flood_drought_EN.csv")
-ori_data.to_excel("./data/flood_drought_EN.xlsx")
+#ori_data.to_csv("./data/flood_drought_EN.csv")
+#ori_data.to_excel("./data/flood_drought_EN.xlsx")
 # In case of API failed. The data would be export to csv
+
+import googlemaps
+gmaps=googlemaps.Client(key="AIzaSyC5OpS9gqtZqG1zWLsqs_sa8CuKysTpt3g")
+# Setup python client for geocoding API
+latitude=[]
+longtitude=[]
+count=0
+for city in citylist_EN:
+    citycoding=gmaps.geocode(city)
+    count+=1
+    #print(count)
+    #print(citycoding)
+    #keylist=citycoding[0].keys()
+    #print(keylist)
+    if not citycoding==[]:
+        city_lat=citycoding[0]["geometry"]["location"]["lat"] # Extract latitude from result   
+        city_lng=citycoding[0]["geometry"]["location"]["lng"] # Extract longtitude from result
+        if 3<city_lat<60 and 70<city_lng<140:   # Select results that located in China
+            latitude.append(city_lat)
+            longtitude.append(city_lng)
+        else:
+            print(city) # Check with city that failed to code and the index of this city
+            index_ll=count-1
+            print(index_ll)
+            latitude.append(0)
+            longtitude.append(0)
+    else:   # Check with city that failed to code and the index of this city
+        print(city)
+        index_ll=count-1
+        print(index_ll)
+        latitude.append(0)
+        longtitude.append(0)
+
+latitude[14]=42.203591
+longtitude[14]=116.485555
+
+latitude[18]=39.08965
+longtitude[18]=107.97616
+
+latitude[26]=38.874434
+longtitude[26]=115.464589
+
+latitude[57]=27.087637
+longtitude[57]=114.964696
+
+latitude[97]=36.585445
+longtitude[97]=109.489757
+
+latitude[98]=34.341574
+longtitude[98]=108.93977
+
+latitude[112]=25.606486
+longtitude[112]=100.267638
+
+latitude[115]=22.78691
+longtitude[115]=100.977164
+
+
+
+datadict={"city":citylist_EN,"latitude":latitude,"longtitude":longtitude} # Setup dictionary to apply DataFrame method
+datareconstruct=DataFrame(datadict, columns=["city","latitude","longtitude"])
+print(datareconstruct)
